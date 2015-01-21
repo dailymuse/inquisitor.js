38e90d4a11911b3a874da5e82815d9e91b2d1464
Yusuf Simonson <simonson@gmail.com>
2015-01-21 14:53:11 -0500

    Added comments

--- examples/json_api.js
+++ examples/json_api.js
@@ -1,12 +1,15 @@
 var catnap = require("./lib");
 var m = catnap.middleware;
 
+// Tests that a simple, JSON-based API returns the proper results
 var test = new catnap.Test("JsonTest");
 
+// Makes the request to the JSON API
 function sendEcho() {
     this.request("http://echo.jsontest.com/key/value/one/two");
 }
 
+// Checks the response
 function receiveEcho() {
     this.deepEqual(this.responseJson, {
         one: "two",
--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,7 @@
 var catnap = require("./lib");
 
+// A simple "hello world" example. This doesn't actually make any HTTP calls,
+// but rather serves as a minimal example.
 module.exports = new catnap.Test(
     "HelloWorldTest",
     new catnap.TestCase("helloWorld", function() {
